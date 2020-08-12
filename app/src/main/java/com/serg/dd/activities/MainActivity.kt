6a1ab8d2ace82f760388c0c55960af6d14bdeb77
Osa-sergey <32779059+Osa-sergey@users.noreply.github.com>
2020-08-12 17:09:01 +0300

    Update MainActivity.kt

--- app/src/main/java/com/serg/dd/activities/MainActivity.kt
+++ app/src/main/java/com/serg/dd/activities/MainActivity.kt
@@ -3,6 +3,10 @@ package com.serg.dd.activities
 import androidx.appcompat.app.AppCompatActivity
 import com.serg.dd.R
 
+/**
+ * Hi
+ */
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
+   var a = 5
 }
+
